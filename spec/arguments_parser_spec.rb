f596837212ce0508d16ec2fb8812dd30686433bd
Ana <anadanieladragan@gmail.com>
2022-03-26 14:35:20 +0200

    Test ArgumentsParser class functionality

---
+++ spec/arguments_parser_spec.rb
@@ -0,0 +1,10 @@
+require './arguments_parser'
+
+describe ArgumentsParser do
+
+    it "get show command with arguments" do
+        argv = ["show", "rails"]
+        expect(ArgumentsParser.get_arguments_string(argv, "show")).to eql ("rails")
+    end
+
+end
