34ba61331d3bec8293f0182123a355292fac9bd2
Ana <anadanieladragan@gmail.com>
2022-03-26 14:01:11 +0200

    Test search functionality implemented

--- spec/command_parser_spec.rb
+++ spec/command_parser_spec.rb
@@ -4,11 +4,18 @@ describe CommandParser do
     include CommandParser
 
     describe "Run program" do
-        it "show rails details" do  
+        it "show rspec details" do  
             rubygems_response = { name: 'rspec' }
             expect(self).to receive(:print_gem).with(rubygems_response)
             expect(self).to receive(:show_gem).with('rspec').and_return(rubygems_response)
             execute_command(["show", "rspec"])
+        end
+
+        it "search ruby gem" do
+            gems_list_response = []
+            expect(self).to receive(:print_filtered_gems_by).with(gems_list_response, "name", "rspec")
+            expect(self).to receive(:search_gems).with('rspec').and_return(gems_list_response)
+            execute_command(["search", "rspec"])
         end
 
     end
