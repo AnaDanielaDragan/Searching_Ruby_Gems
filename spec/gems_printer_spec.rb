3994f3f40b6832416f17282527ae55f2419ad94b
Ana <anadanieladragan@gmail.com>
2022-03-29 19:14:31 +0300

    Created tests for printing methods in GemsPrinter

--- spec/gems_printer_spec.rb
+++ spec/gems_printer_spec.rb
@@ -1,14 +1,31 @@
 require './lib/gems_printer'
 
 describe GemsPrinter do
+    subject(:print_class) {GemsPrinter}
 
-    describe "print a gem" do
+    let(:gem_info) { ['name' => 'rails', 'info' => 'rails info here'] }
 
-        it "prints rails gem" do
-            gem = {"name" => "rails", "info" => "rails info here"}
-            #expect(GemsPrinter.print_gem(gem))
+    describe '.print_gem' do
+        let(:result) { {"name"=>"rails", "info"=>"rails info here"}}
+
+        context 'prints rails gem info' do
+            it 'contains given info' do
+                expect(print_class.print_gem(gem_info)).to include(result)
+            end
         end
+    end
 
+    describe '.print_filtered_gems_by' do
+        subject(:print_gems_by) { print_class.print_gems_by(gems, "name")}
+
+        let(:gems) { [] }
+
+        context 'prints a gem list' do
+            it 'calls print_gems_by method' do
+                expect(GemsPrinter).to respond_to(:print_gems_by).with(2).argument
+
+            end
+        end
     end
 
 end
