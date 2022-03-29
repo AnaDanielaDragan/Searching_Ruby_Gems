406904ed56e0e64dd591335e1d0b2c014865fb5b
Ana <anadanieladragan@gmail.com>
2022-03-29 17:05:31 +0300

    Implemented general class for handling commands

---
+++ lib/command_handler.rb
@@ -0,0 +1,17 @@
+class CommandHandler
+    Faraday.default_adapter = :net_http
+
+    def initialize(arguments_string)
+        @arguments_string = arguments_string
+    end
+
+    def handle_command
+        HelpCommandHandler.new
+    end
+
+    def get_from_url(url)
+        response = Faraday.get(url)
+        print url
+        result = JSON.parse(response.body)
+    end
+end
