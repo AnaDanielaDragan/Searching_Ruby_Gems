9c06b455d6c35ae08033fc96d4801e5d390d6d72
Ana <anadanieladragan@gmail.com>
2022-03-29 17:05:03 +0300

    Implemented class for handling the show command

--- lib/show_command_handler.rb
+++ lib/show_command_handler.rb
@@ -1,21 +1,11 @@
-class ShowCommandHandler
-    Faraday.default_adapter = :net_http
+require './lib/command_handler'
 
-    def initialize(arguments_string)
-        @arguments_string = arguments_string
-    end
-
+class ShowCommandHandler < CommandHandler
     def handle_command
         @gem_info = get_from_url("https://rubygems.org/api/v1/gems/#{@arguments_string}.json")
     end
 
     def print_result
         GemsPrinter.print_gem(@gem_info)
-    end
-
-    def get_from_url(url)
-        response = Faraday.get(url)
-        print url
-        result = JSON.parse(response.body)
     end
 end
