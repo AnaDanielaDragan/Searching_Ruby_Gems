4f110e9c6eff0cf786b84e10aec8b41aa2aa3504
Ana <anadanieladragan@gmail.com>
2022-04-01 20:22:18 +0300

    Refactored printing class to print the generated result

--- lib/result_printer.rb
+++ lib/result_printer.rb
@@ -1,29 +1,32 @@
 class ResultPrinter
-  def self.print(result)
-    case result.keys
-    when 'message' then print_message(result)
-    when 'gems_list' then print_gems_by(result.values, 'name')
-    when 'gem_info' then print_gem(result.values)
-    else print_message('Nothing to print.')
-  end
-
-  def print_gem(gem_info)
-    gem_info.each do |label, info|
-      puts "#{label}: #{info}"
-    end
-  end
-
-  def print_gems_by(gems_list, label_name)
-    gems_list.each do |gem|
-      gem.each do |label, info|
-        puts info if label == label_name
+  class << self
+    def print(result)
+      case result[0]
+      when 'message' then print_message(result[1])
+      when 'gems_list' then print_gems_by(result[1], 'name')
+      when 'gem_info' then print_gem(result[1])
+      else print_message('Nothing to print.')
       end
     end
 
-    puts "Number of gems found: #{gems_list.length}"
-  end
+    def print_gem(gem_info)
+      gem_info.each do |label, info|
+        puts "#{label}: #{info}"
+      end
+    end
 
-  def print_message(message_string)
-    puts message_string
+    def print_gems_by(gems_list, label_name)
+      gems_list.each do |gem|
+        gem.each do |label, info|
+          puts info if label == label_name
+        end
+      end
+
+      puts "Number of gems found: #{gems_list.length}"
+    end
+
+    def print_message(message_string)
+      puts message_string
+    end
   end
 end
