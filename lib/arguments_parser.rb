7766b8ccaeb952956a9c0fc1c7c0918dbf245b28
Ana <anadanieladragan@gmail.com>
2022-04-01 20:23:25 +0300

    Refactoerd class to a single functionality, that of parsing an arguments array

--- lib/arguments_parser.rb
+++ lib/arguments_parser.rb
@@ -1,25 +1,8 @@
-require './lib/search_command_handler'
-require './lib/show_command_handler'
-require './lib/command_handler_context'
-
 class ArgumentsParser
-  def initialize(arguments)
-    @arguments = arguments
-    @context_handler = CommandHandlerContext.new
-  end
+  def self.parse(arguments)
+    @command_string = arguments[0]
+    @arguments_string = arguments[1..].join(' ')
 
-  def parse
-    @command_string = @arguments[0]
-    @arguments_string = @arguments[1..].join(' ')
-  end
-
-  def execute
-    case @command_string
-    when 'show' then @context_handler.set_command_handler(ShowCommandHandler.new(@arguments_string))
-    when 'search' then @context_handler.set_command_handler(SearchCommandHandler.new(@arguments_string))
-    else puts 'No command given.'
-    end
-
-    @context_handler.execute
+    [@command_string, @arguments_string]
   end
 end
