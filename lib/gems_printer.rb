574da38fe917dd4917b411174be4d11444f6ef24
Ana <anadanieladragan@gmail.com>
2022-04-01 15:43:45 +0300

    Deleted unnecessary filter functionality for gem printing

--- lib/gems_printer.rb
+++ lib/gems_printer.rb
@@ -5,33 +5,14 @@ class GemsPrinter
     end
   end
 
-  def self.print_filtered_gems_by(gems_list, label_name, search_string)
-    gems_found_by_name_search = []
-    gems_found_by_info_search = []
-
-    gems_list.each do |gem|
-      gem.each do |label, info|
-        if label == 'name'
-          info.include?(search_string) ? gems_found_by_name_search.push(gem) : gems_found_by_info_search.push(gem)
-        end
-      end
-    end
-
-    puts 'Gems found by name:'
-    print_gems_by(gems_found_by_name_search, label_name)
-
-    puts 'Gems found by info:'
-    print_gems_by(gems_found_by_info_search, label_name)
-
-    puts "Number of gems found: #{gems_list.length}"
-  end
-
   def self.print_gems_by(gems_list, label_name)
     gems_list.each do |gem|
       gem.each do |label, info|
         puts info if label == label_name
       end
     end
+
+    puts "Number of gems found: #{gems_list.length}"
   end
 end
 
