d85fce96bdb09e846dd35a34a833085813c5d61a
Ana <anadanieladragan@gmail.com>
2022-04-01 19:22:18 +0300

    Implemented class to handle the http connection

---
+++ lib/http_client.rb
@@ -0,0 +1,29 @@
+class HTTPClient
+  Faraday.default_adapter = :net_http
+
+  def self.get_gems_list(search_string)
+    page = 1
+
+    result = get_from_url("https://rubygems.org/api/v1/search.json?query=#{search_string}&page=#{page}")
+
+    until get_from_url("https://rubygems.org/api/v1/search.json?query=#{search_string}&page=#{page}").empty?
+      result.concat(get_from_url("https://rubygems.org/api/v1/search.json?query=#{search_string}&page=#{page}"))
+      page += 1
+
+      # change search restictions here
+      break if result.length > 10
+    end
+    result
+  end
+
+  def self.get_gem_info(search_string)
+    get_from_url("https://rubygems.org/api/v1/gems/#{search_string}.json")
+  end
+
+  private
+
+  def self.get_from_url(url)
+    response = Faraday.get(url)
+    JSON.parse(response.body)
+  end
+end
