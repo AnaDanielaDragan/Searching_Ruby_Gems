9d22c7a01cfb84c4d2a3e8d5b0829dcf934691b8
Ana <anadanieladragan@gmail.com>
2022-04-02 16:38:45 +0300

    Implemented options to search functionality

--- lib/search_command_handler.rb
+++ lib/search_command_handler.rb
@@ -1,8 +1,17 @@
 require './lib/ruby_gems_client'
-require 'optparse'
+require './lib/search_options_parser'
+require './lib/gem_info'
+require './lib/options_handler'
 
 class SearchCommandHandler
   def self.execute(arguments_string)
-    RubyGemsClient.search(arguments_string)
+    gems_list = RubyGemsClient.search(arguments_string.split.first)
+                              .map { |gem| GemInfo.new(gem) }
+
+    options = SearchOptionsParser.parse(arguments_string)
+
+    #gems_list = options[0].apply(gems_list)
+
+    OptionsHandler.apply(options, gems_list)
   end
 end
