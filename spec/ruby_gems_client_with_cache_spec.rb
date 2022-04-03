b5d168734cd5b53592b6326bf6ac696d1be56107
Ana <anadanieladragan@gmail.com>
2022-04-03 19:58:41 +0300

    Implemented tests for RubyGemsClientWithCache class

---
+++ spec/ruby_gems_client_with_cache_spec.rb
@@ -0,0 +1,45 @@
+require './lib/ruby_gems_client_with_cache'
+
+RSpec.describe RubyGemsClientWithCache do
+  subject(:client) { RubyGemsClientWithCache }
+
+  describe '.search' do
+    subject(:search) { client.search(search_string) }
+
+    context 'when searching with no parameters' do
+      let(:search_string) { '' }
+
+      it 'returns an empty object' do
+        expect(search).to be_empty
+      end
+    end
+
+    context 'when searching for rails gem' do
+      let(:search_string) { 'rails' }
+
+      it 'returns a JSON object containing one or multiple objects' do
+        expect(search.length).to be >= 1
+      end
+    end
+  end
+
+  describe '.info' do
+    subject(:info) { client.info(gem_name) }
+
+    context 'when requesting gem info with no parameters' do
+      let(:gem_name) { '' }
+
+      it 'returns an empty object' do
+        expect(info).to be_empty
+      end
+    end
+
+    context 'when requesting infor about rails' do
+      let(:gem_name) { 'rails' }
+
+      it 'returns a JSON object containing name => rails' do
+        expect(info).to include('name' => 'rails')
+      end
+    end
+  end
+end
