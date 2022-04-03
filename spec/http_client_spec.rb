df13611421908df8152ad0d348c5ea2039e7c57d
Ana <anadanieladragan@gmail.com>
2022-04-03 19:34:07 +0300

    Implemented tests for no parameters given case

--- spec/http_client_spec.rb
+++ spec/http_client_spec.rb
@@ -8,6 +8,14 @@ RSpec.describe HTTPClient do
   describe '.get_gems_list' do
     subject(:get_gems_list) { http_client.get_gems_list(search_string) }
 
+    context 'when giving no parameters' do
+      let(:search_string) { '' }
+
+      it 'returns an empty object' do
+        expect(get_gems_list).to be_empty
+      end
+    end
+
     context 'when requesting a gem' do
       let(:search_string) { 'rspec' }
 
@@ -19,6 +27,14 @@ RSpec.describe HTTPClient do
 
   describe '.get_gem_info' do
     subject(:get_gem_info) { http_client.get_gem_info(search_string) }
+
+    context 'when giving no parameters' do
+      let(:search_string) { '' }
+
+      it 'returns an empty object' do
+        expect(get_gem_info).to be_empty
+      end
+    end
 
     context 'searches for rails gem' do
       let(:search_string) { 'rails' }
