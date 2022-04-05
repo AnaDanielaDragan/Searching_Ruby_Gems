0a888dfa720659c6272211b3a83bf8fdf2aea246
Ana <anadanieladragan@gmail.com>
2022-04-05 21:16:56 +0300

    Added parameter to store complete info

--- lib/gem_info.rb
+++ lib/gem_info.rb
@@ -1,12 +1,13 @@
 require 'pry'
 
 class GemInfo
-  attr_reader :name, :info, :downloads, :licenses
+  attr_reader :name, :info, :downloads, :licenses, :gem_info
 
   def initialize(gem_info)
     @name = gem_info['name']
     @info = gem_info['info']
     @downloads = gem_info['downloads']
     @licenses = gem_info['licenses']
+    @gem_info = gem_info
   end
 end
