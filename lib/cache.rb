f412a108bc956a1df9bb1049c7e2c3e964b83328
Ana <anadanieladragan@gmail.com>
2022-04-06 21:38:21 +0300

    Created Cache class to handle cached data from database

---
+++ lib/cache.rb
@@ -0,0 +1,49 @@
+require 'sqlite3'
+require 'time'
+
+class Cache
+  DEFAULT_CACHE_EXPIRATION_TIME = 48 * 60 * 60
+
+  def self.create
+    @instance ||= new
+  end
+
+  def initialize
+    @db = SQLite3::Database.open 'CACHE.db'
+    @db.execute 'CREATE TABLE IF NOT EXISTS cache(key VARCHAR(16) PRIMARY KEY, expire_date TEXT, value TEXT)'
+  end
+
+  def read(key)
+    @db = SQLite3::Database.open 'CACHE.db'
+    results = @db.query 'SELECT expire_date, value FROM cache WHERE key=?', key
+    first_result = results.next
+
+    return unless first_result
+
+    parsed_expire_date = Time.parse(first_result[0])
+    parsed_now = Time.parse(Time.now.iso8601)
+
+    if parsed_expire_date > parsed_now
+      puts 'Raw text from database:'
+      puts first_result[1]
+      #parse text to return json
+      JSON.parse(first_result[1].to_json)
+    end
+
+    # should return a JSON object
+  end
+
+  def write(key, value)
+    # value is a JSON object
+    return if value.nil?
+
+    expire_date = Time.now + DEFAULT_CACHE_EXPIRATION_TIME
+    @db = SQLite3::Database.open 'CACHE.db'
+    @db.execute 'INSERT INTO cache (key, expire_date, value) VALUES (?, ?, ?)',
+                key, expire_date.iso8601, JSON.dump(value)
+  end
+
+  def close
+    @db.close if @db
+  end
+end
---
+++ spec/cache_spec.rb
@@ -0,0 +1,17 @@
+require './lib/cache'
+
+RSpec.describe Cache do
+  subject(:cache) { Cache.create }
+
+  describe '#read' do
+    subject(:read) { cache.read(key) }
+
+    context 'reading an entry' do
+    end
+  end
+
+  # Question: How to test this class?
+  # Ideas:
+  #   - Mock the database
+  #   - Add test entry at the beginning and remove it at the end
+end
